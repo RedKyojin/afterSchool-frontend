4b298576eb689156ad1c6ad0bf81b13bed54aa54
Faizan <faizan579555@gmail.com>
2025-12-11 07:24:44 +0000

    Displayed Lessons

---
+++ script.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            //Creating the lessons
+            lessons: [
+                {
+                    id: 1,
+                    subject: "Maths",
+                    location: "M5",
+                    price: 20,
+                    spaces: 9,
+                    icon: "➕"
+                },
+                {
+                    id: 2,
+                    subject: "Science",
+                    location: "S2",
+                    price: 20,
+                    spaces: 12,
+                    icon: "🧑‍🔬"
+                },
+                {
+                    id: 3,
+                    subject: "Chess",
+                    location: "R6",
+                    price: 25,
+                    spaces: 13,
+                    icon: "♟️"
+                }
+                //I realise I need at least 10; going to add more later.
+            ]
+        };
+    }
+});
+
+app.mount("#app");
