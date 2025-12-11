50d6386761e99427bdd50bf87e7ba9a178baf891
Faizan <faizan579555@gmail.com>
2025-12-11 11:58:30 +0000

    Checkout

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            //Creating the lessons
+            // Creating the lessons
             lessons: [
                 {
                     id: 1,
@@ -9,7 +9,7 @@ const app = Vue.createApp({
                     location: "M5",
                     price: 20,
                     spaces: 9,
-                    icon: "➕"
+                    icon: "✖️"
                 },
                 {
                     id: 2,
@@ -27,17 +27,40 @@ const app = Vue.createApp({
                     spaces: 13,
                     icon: "♟️"
                 }
-                //I realise I need at least 10; going to add more later.
+                // I realise I need at least 10; going to add more later.
             ],
-            cart: [], //Array to hold the ids
+            cart: [], // Array to hold the ids
+            showCart: false // Boolean for checkout
         };
+    },
+    computed: {
+        cartItems() {
+            return this.lessons.filter(lesson =>
+                this.cart.includes(lesson.id)
+            );
+        }
     },
     methods: {
         addToCart(lesson) {
-            if(lesson.spaces > 0) {
+            if (lesson.spaces > 0) {
                 this.cart.push(lesson.id);
                 lesson.spaces--;
-            };
+            }
+        },
+        removeFromCart(lessonId) {
+            const index = this.cart.indexOf(lessonId);
+            if (index !== -1) {
+                // Removes from cart
+                this.cart.splice(index, 1);
+                // Search the lessons array, and return the first lesson where its id equals lessonId
+                const lesson = this.lessons.find(l => l.id === lessonId);
+                if (lesson) {
+                    lesson.spaces++;
+                }
+            }
+        },
+        toggleView() {
+            this.showCart = !this.showCart;
         }
     }
 });
